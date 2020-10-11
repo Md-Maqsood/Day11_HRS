6f3d081ad0d7a3126b8734ad635de7eaca88ac79
maqsood <maqsoodhamd37@gmail.com>
2020-10-11 20:58:50 +0530

    [Maqsood].update: added the ability to find the chaepest and best rated hotel given customer type and date range in the input

--- src/main/java/com/BridgeLabs/hotelReservationSystem/HotelReservation.java
+++ src/main/java/com/BridgeLabs/hotelReservationSystem/HotelReservation.java
@@ -15,20 +15,25 @@ import java.util.stream.Collectors;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.BridgeLabs.hotelReservationSystem.HotelReservation.CustomerType;
+
 public class HotelReservation {
+	
+	public enum CustomerType{
+		REGULAR, REWARDS
+	}
+	
 	private static final Logger logger = LogManager.getLogger(HotelReservation.class);
 	static Scanner sc = new Scanner(System.in);
 	public List<Hotel> hotels;
 	private static final Pattern DAY_PATTERN = Pattern.compile("[0-9]{2}[A-Z][a-z]{2}[0-9]{4}");
+	private static final Pattern CUSTOMER_TYPE_PATTERN=Pattern.compile("Re[gw][ua][lr][ad][rs]");
 	private static final List<DayOfWeek> WEEKENDS = Arrays.asList(new DayOfWeek[] { DayOfWeek.SUNDAY, DayOfWeek.SATURDAY });
 
 	public HotelReservation() {
 		this.hotels = new ArrayList<Hotel>();
 	}
 
-	/**
-	 * uc9
-	 */
 	public void addHotels() {
 		do {
 			logger.debug("Enter the hotel details in given order -\nName:\nWeekday Rate for Regular Customer:\nWeekend Rate for Regular Customer:\nWeekday Rate for Rewards Customer:\nWeekend Rate for Rewards Customer:\nRating:");
@@ -38,15 +43,21 @@ public class HotelReservation {
 		} while (sc.nextLine().equals("1"));
 	}
 
-	/**
-	 * uc12
-	 */
-	public void getCheapestBestRatedHotelForRegular() {
-		Customer customer=getCustomerInput();
+	public int getTotalRate(Hotel hotel,Customer customer) throws NullPointerException{
 		int numWeekdays=customer.getNumWeekdays();
 		int numWeekends=customer.getNumWeekends();
+		int weekdayRate = customer.getType() == CustomerType.REWARDS ? hotel.getRewardsWeekdayRate()
+				: hotel.getRegularWeekdayRate();
+		int weekendRate = customer.getType() == CustomerType.REWARDS ? hotel.getRewardsWeekendRate()
+				: hotel.getRegularWeekendRate();
+		int totalRate = weekdayRate * numWeekdays + weekendRate * numWeekends;
+		return totalRate;
+	}
+	
+	public void getCheapestBestRatedHotel() {
+		Customer customer=getCustomerInput();
 		Map<Hotel, Integer> hotelToTotalRateMap = hotels.stream().collect(Collectors.toMap(hotel -> hotel,
-				hotel -> hotel.getRegularWeekendRate() * numWeekends + hotel.getRegularWeekdayRate() * numWeekdays));
+				hotel -> getTotalRate(hotel, customer)));
 		Hotel cheapestBestRatedHotel = hotelToTotalRateMap.keySet().stream().min((hotel1, hotel2) -> {
 			int rateDifference = hotelToTotalRateMap.get(hotel1) - hotelToTotalRateMap.get(hotel2);
 			int ratingDifference = hotel1.getRating() - hotel2.getRating();
@@ -59,37 +70,17 @@ public class HotelReservation {
 			logger.debug("No hotel found");
 		}
 	}
-	
-	/**
-	 * uc11
-	 */
-	public void getCheapestBestRatedHotelForRewards() {
-		Customer customer=getCustomerInput();
-		int numWeekdays=customer.getNumWeekdays();
-		int numWeekends=customer.getNumWeekends();
-		Map<Hotel, Integer> hotelToTotalRateMap = hotels.stream().collect(Collectors.toMap(hotel -> hotel,
-				hotel -> hotel.getRewardsWeekendRate() * numWeekends + hotel.getRewardsWeekdayRate() * numWeekdays));
-		Hotel cheapestBestRatedHotel = hotelToTotalRateMap.keySet().stream().min((hotel1, hotel2) -> {
-			int rateDifference = hotelToTotalRateMap.get(hotel1) - hotelToTotalRateMap.get(hotel2);
-			int ratingDifference = hotel1.getRating() - hotel2.getRating();
-			return rateDifference == 0 ? -(ratingDifference) : rateDifference;
-		}).orElse(null);
-		try {
-			logger.debug(cheapestBestRatedHotel.getName() + ", Rating: " + cheapestBestRatedHotel.getRating() + " and Total Rates: $"
-					+ hotelToTotalRateMap.get(cheapestBestRatedHotel));
-		} catch (NullPointerException e) {
-			logger.debug("No hotel found");
-		}
-	}
-	
-	/**
-	 * uc11
-	 * Method takes date range input from customer and returns Customer object
-	 * @return
-	 */
+
 	public Customer getCustomerInput() {
-		logger.debug("Enter the date range in format <date1>, <date2>, <date3>\nEg.:  09Mar2020, 10Mar2020, 11Mar2020");
+		logger.debug("Enter the date range in format <customer_type>:<date1>, <date2>, <date3>\nEg.:  Regular: 09Mar2020, 10Mar2020, 11Mar2020");
 		String customerInput = sc.nextLine();
+		Matcher customerTypeMatcher=CUSTOMER_TYPE_PATTERN.matcher(customerInput);
+		CustomerType customerType=CustomerType.REGULAR;
+		if(customerTypeMatcher.find()) {
+			if(customerTypeMatcher.group().equals("Rewards")) {
+				customerType=CustomerType.REWARDS;
+			}
+		}
 		Matcher dateMatcher = DAY_PATTERN.matcher(customerInput);
 		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("ddMMMuuuu");
 		List<DayOfWeek> daysList = new ArrayList<DayOfWeek>();
@@ -97,21 +88,15 @@ public class HotelReservation {
 			daysList.add(LocalDate.parse(dateMatcher.group(),formatter).getDayOfWeek());
 		}
 		int numWeekends = (int) daysList.stream().filter(day -> WEEKENDS.contains(day)).count();
-		return new Customer(daysList.size()-numWeekends, numWeekends);
+		return new Customer(daysList.size()-numWeekends, numWeekends, customerType);
 	}
 	
-	/**
-	 * uc7
-	 */
 	public void getBestRatedHotel() {
 		Customer customer=getCustomerInput();
-		int numWeekdays=customer.getNumWeekdays();
-		int numWeekends=customer.getNumWeekends();
 		Hotel bestRatedHotel = hotels.stream().max((hotel1, hotel2) -> hotel1.getRating() - hotel2.getRating())
 				.orElse(null);
 		try {
-			int totalRate = bestRatedHotel.getRegularWeekdayRate() * numWeekdays
-					+ bestRatedHotel.getRegularWeekendRate() * numWeekends;
+			int totalRate=getTotalRate(bestRatedHotel, customer);
 			logger.debug(bestRatedHotel.getName() + " & Total Rates $" + totalRate);
 		} catch (NullPointerException e) {
 			logger.debug("No hotel found");
@@ -121,17 +106,26 @@ public class HotelReservation {
 	public static void main(String[] args) {
 		HotelReservation hotelReservation = new HotelReservation();
 		hotelReservation.addHotels();
-		hotelReservation.getCheapestBestRatedHotelForRegular();
+		hotelReservation.getCheapestBestRatedHotel();
 	}
 }
 
 class Customer{
 	private int numWeekdays;
 	private int numWeekends;
-	public Customer(int numWeekdays, int numWeekends) {
+	private CustomerType type;
+	
+	public Customer(int numWeekdays, int numWeekends, CustomerType type) {
 		super();
 		this.numWeekdays = numWeekdays;
 		this.numWeekends = numWeekends;
+		this.type = type;
+	}
+	public CustomerType getType() {
+		return type;
+	}
+	public void setType(CustomerType type) {
+		this.type = type;
 	}
 	public int getNumWeekdays() {
 		return numWeekdays;
