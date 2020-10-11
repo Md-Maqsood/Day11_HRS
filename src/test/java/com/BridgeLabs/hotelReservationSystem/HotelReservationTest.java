3655f95bcf2d80214de0d356f4df5f27cf7bfa8b
maqsood <maqsoodhamd37@gmail.com>
2020-10-11 09:36:22 +0530

    first commit

---
+++ src/test/java/com/BridgeLabs/hotelReservationSystem/HotelReservationTest.java
@@ -0,0 +1,20 @@
+package com.BridgeLabs.hotelReservationSystem;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HotelReservationTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
