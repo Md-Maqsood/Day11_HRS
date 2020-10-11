38d152cd2fb3355ac2e2202340006b05f690ddd6
maqsood <maqsoodhamd37@gmail.com>
2020-10-11 14:47:26 +0530

    [Maqsood].update: added the feature to find the cheapest hotel for a given date Range

--- src/main/java/com/BridgeLabs/hotelReservationSystem/HotelReservation.java
+++ src/main/java/com/BridgeLabs/hotelReservationSystem/HotelReservation.java
@@ -2,7 +2,11 @@ package com.BridgeLabs.hotelReservationSystem;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,7 +15,7 @@ public class HotelReservation {
 	private static final Logger logger=LogManager.getLogger(HotelReservation.class);
 	static Scanner sc=new Scanner(System.in);
 	public List<Hotel> hotels;
-	
+	private static final Pattern DAY_PATTERN = Pattern.compile("\\([a-z]{3,4}\\)");
 	public HotelReservation() {
 		this.hotels=new ArrayList<Hotel>();
 	}
@@ -27,10 +31,26 @@ public class HotelReservation {
 		}while(sc.nextLine().equals("1"));
 	}
 	
+	/**
+	 * uc2
+	 */
+	public void getCheapestHotel() {
+		logger.debug("Enter the date range in format <date1>, <date2>, <date3>\nEg.:  16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)");
+		String customerInput=sc.nextLine();
+		Matcher dayMatcher=DAY_PATTERN.matcher(customerInput);
+		List<String> daysList= new ArrayList<String>();
+		while(dayMatcher.find()) {
+			daysList.add(dayMatcher.group());
+		}
+		Map<Hotel, Integer> hotelToTotalRateMap=hotels.stream().collect(Collectors.toMap(h->h, h->h.getRegularRate()*daysList.size()));
+		Hotel cheapestHotel=hotelToTotalRateMap.keySet().stream().min((n1,n2)->hotelToTotalRateMap.get(n1)-hotelToTotalRateMap.get(n2)).orElse(null);
+		logger.debug(cheapestHotel.getName()+", Total Rates: $"+hotelToTotalRateMap.get(cheapestHotel));
+	}
+	
 	public static void main(String[] args) {
 		HotelReservation hotelReservation=new HotelReservation();
 		hotelReservation.addHotels();
-		hotelReservation.hotels.forEach(n->logger.debug(n));
+		hotelReservation.getCheapestHotel();
 	}
 }
 
